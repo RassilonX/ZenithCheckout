6166c45fbaff0113471211e8dde8a61f0fd48daa
RassilonX <chrisfraser18@gmail.com>
2024-08-01 12:23:22 +0100

    Added special price logic

--- ZenithCheckout/Checkout/Checkout.cs
+++ ZenithCheckout/Checkout/Checkout.cs
@@ -50,7 +50,18 @@ public class Checkout : ICheckout
     {
         int total = 0;
 
-        total += _itemPrices[item] * amount;
+        if (_specialPrices.ContainsKey(item))
+        {
+            int sets = amount / _specialPrices[item]._setAmount;
+            int specialTotal = sets * _specialPrices[item]._setPrice;
+            int remainder = amount % _specialPrices[item]._setAmount;
+
+            total += specialTotal + _itemPrices[item] * remainder;
+        }
+        else
+        {
+            total += _itemPrices[item] * amount;
+        }
 
         return total;
     }
