f43bb3383f113744620900b8ea5e1d9a21cbe8e4
RassilonX <chrisfraser18@gmail.com>
2024-08-02 09:49:50 +0100

    Added checkout factory and basic testing

---
+++ ZenithCheckout/Checkout/CheckoutFactory.cs
@@ -0,0 +1,33 @@
+﻿using CheckoutLibrary.Interfaces;
+using CheckoutLibrary.Models;
+using CheckoutLibrary;
+using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("CheckoutFactoryTests")]
+namespace CheckoutLibrary;
+
+public class CheckoutFactory
+{
+    private static readonly Dictionary<string, ICheckout> _checkouts = new Dictionary<string, ICheckout>();
+
+    public static void RegisterCheckouts(IServiceCollection services, 
+        params (string name, Dictionary<char, int> itemPrices, Dictionary<char, SpecialPrice> specialPrices)[] checkouts)
+    {
+        foreach (var (name, itemPrices, specialPrices) in checkouts)
+        {
+            _checkouts.Add(name, new Checkout(itemPrices, specialPrices));
+        }
+
+        services.AddSingleton(_checkouts);
+    }
+
+    public static ICheckout GetService(IServiceProvider serviceProvider, string name)
+    {
+        var checkouts = serviceProvider.GetService<Dictionary<string, ICheckout>>();
+        return checkouts[name];
+    }
+
+    //Test helper methods
+    internal static void ResetCheckoutDictionary() { _checkouts.Clear(); }
+}
---
+++ ZenithCheckout/CheckoutTests/CheckoutFactoryTests.cs
@@ -0,0 +1,73 @@
+﻿using CheckoutLibrary;
+using CheckoutLibrary.Interfaces;
+using CheckoutLibrary.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CheckoutTests;
+
+public class CheckoutFactoryTests
+{
+    [Fact]
+    public void RegisterCheckouts_RegistersCheckoutsCorrectly()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        var checkouts = new[]
+        {
+            ("default", new Dictionary<char, int> { { 'A', 10 } }, new Dictionary<char, SpecialPrice> { { 'A', new SpecialPrice(3, 20) } }),
+            ("premium", new Dictionary<char, int> { { 'B', 20 } }, new Dictionary<char, SpecialPrice> { { 'B', new SpecialPrice(2, 30) } }),
+        };
+
+        // Act
+        CheckoutFactory.RegisterCheckouts(services, checkouts);
+
+        // Assert
+        var serviceProvider = services.BuildServiceProvider();
+        var registeredCheckouts = serviceProvider.GetService<Dictionary<string, ICheckout>>();
+        Assert.NotNull(registeredCheckouts);
+        Assert.True(registeredCheckouts.ContainsKey("default"));
+        Assert.True(registeredCheckouts.ContainsKey("premium"));
+
+        CheckoutFactory.ResetCheckoutDictionary();
+    }
+
+    [Fact]
+    public void GetService_ReturnsCorrectCheckoutInstance()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        var checkouts = new[]
+        {
+            ("default", new Dictionary<char, int> { { 'A', 10 } }, new Dictionary<char, SpecialPrice> { { 'A', new SpecialPrice(3, 20) } }),
+        };
+        CheckoutFactory.RegisterCheckouts(services, checkouts);
+
+        // Act
+        var serviceProvider = services.BuildServiceProvider();
+        var defaultCheckout = CheckoutFactory.GetService(serviceProvider, "default");
+
+        // Assert
+        Assert.NotNull(defaultCheckout);
+        Assert.IsAssignableFrom<Checkout>(defaultCheckout);
+
+        CheckoutFactory.ResetCheckoutDictionary();
+    }
+
+    [Fact]
+    public void GetService_ThrowsException_WhenCheckoutNameIsNotFound()
+    {
+        // Arrange
+        var services = new ServiceCollection();
+        var checkouts = new[]
+        {
+            ("default", new Dictionary<char, int> { { 'A', 10 } }, new Dictionary<char, SpecialPrice> { { 'A', new SpecialPrice(3, 20) } }),
+        };
+        CheckoutFactory.RegisterCheckouts(services, checkouts);
+
+        // Act and Assert
+        var serviceProvider = services.BuildServiceProvider();
+        Assert.Throws<KeyNotFoundException>(() => CheckoutFactory.GetService(serviceProvider, "non-existent"));
+
+        CheckoutFactory.ResetCheckoutDictionary();
+    }
+}
